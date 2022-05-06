ffcf42ae94a43468dc72df62b4ba20141a10198f
zhanggaolei <zhanggaolei@qq.com>
2022-05-06 15:52:58 +0800

    .

--- jd_jinli_hongbao.ts
+++ jd_jinli_hongbao.ts
@@ -33,25 +33,9 @@
    await open(0)
  })()
  function  random (nBytes) {
-   var words = [];
-   var r = (function (m_w) {
-       var m_w = m_w;
-       var m_z = 0x3ade68b1;
-       var mask = 0xffffffff;
-       return function () {
-           m_z = (0x9069 * (m_z & 0xFFFF) + (m_z >> 0x10)) & mask;
-           m_w = (0x4650 * (m_w & 0xFFFF) + (m_w >> 0x10)) & mask;
-           var result = ((m_z << 0x10) + m_w) & mask;
-           result /= 0x100000000;
-           result += 0.5;
-           return result * (Math.random() > .5 ? 1 : -1);
-       }
-   });
-   for (var i = 0, rcache; i < nBytes; i += 4) {
-       var _r = r((rcache || Math.random()) * 0x100000000);
-       rcache = _r() * 0x3ade67b7;
-       words.push((_r() * 0x100000000) | 0);
-   } 
+   let words='';
+   var n= Math.random()* 0x10000000;
+   words=n.toString().split('.')[0].substring(0,8)
    return words;
  }
  async function join() {
